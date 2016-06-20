5842d4badb4ba3a03873ac6f61ca7d73adef49f0
rene-justin-dado <justindado@yahoo.com>
2016-06-20 20:11:54 +1200

    added getRow function, untested

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+// An object of an array of objects: To track the cells of our game board
+var board = {
+
+  cells: [
+  ]
+}
+
 function startGame () {
   // Get each cell from the board
   addListeners(document.getElementsByClassName('board')[0].children)
@@ -22,3 +29,20 @@ function markCell (evt) {
   evt.preventDefault()
   evt.target.classList.toggle('marked')
 }
+
+// Checks Position on board
+function getRow (element) {
+  // Loop through the 'board' elements
+  for (var i = 0; i < element.length; i++) {
+    if (element[i].classList.contains('row-')) {
+      return (element[i].split('-'))[1]
+    }
+  }
+}
+
+//  As you may remember, each DOM element has a classList property. We can loop through that just as if it were an ordinary array. Do that: write a for loop that loops through each class name in turn.
+
+//  Figure out how to tell if the class name you're currently on begins with row-. If it does, get the number from the end. There are a few ways to do this: one is using the .split function.
+
+//  Return the number once you've found it.
+// For example, if the class were row-9, you'd return 9.
